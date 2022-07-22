3cad68f28f34087af02faa515cf354d3883d20ef
gabrielricardosilva <gabrielricardo.silva17@gmail.com>
2022-07-22 10:45:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/servicolimpeza/appservlimp/EmptyMainActivity3.java
@@ -0,0 +1,14 @@
+package br.com.servicolimpeza.appservlimp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptyMainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_main3);
+    }
+}
